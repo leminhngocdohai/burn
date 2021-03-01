dee481bb5ec2b8b988c2d4d27cdd29cb5d9f5867
Caohue198 <caohe198@gmail.com>
2021-03-01 16:05:45 +0700

    banner

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,9 +1,14 @@
 var config = {
     paths: {
-        'nav-tag': "js/nav-tag"
+        'nav-tag': "js/nav-tag",
+        'accordion': "js/accordion"
     },
     shim: {
         'nav-tag': {
+            deps: ['jquery']
+        },
+
+        'accordion': {
             deps: ['jquery']
         }
     }
